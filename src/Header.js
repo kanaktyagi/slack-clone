84e064321292dcc85776a76c9be1ba1bf218465e
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-21 18:42:56 +0530

    Added the header component

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+import {Avatar} from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <div className="header">
+          <div className="header__left">
+          <Avatar
+            className="header__avatar"
+            // alt={user?.displayName}
+            // src={user?.photoURL}
+            />
+          <AccessTimeIcon />
+          {/* Avatar*/}
+          {/* Time Icon */}
+          </div>  
+          <div className="header__search">
+          < SearchIcon/>
+          <input placeholder="search"/>
+          {/* Search Icon*/}
+          {/* Input */}
+          </div>
+          <div className="header__right">
+          <HelpOutlineIcon />
+          {/* help icon*/}
+          </div>
+        </div>
+    )
+}
+
+export default Header
