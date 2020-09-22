3e145ff4d8a76236cc6a5bbd6e594eb0ffcb413f
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-22 12:47:15 +0530

    Added the Sidebar Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="app">
     <Header />
-     {/* Header */}
-     { /* Sidebar */}
+    <div className="app__body">
+    <Sidebar/>
+    </div>
+   
      { /* React-router*/}
     </div>
   );
